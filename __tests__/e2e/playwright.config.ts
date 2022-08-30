918e899ac724d24b082ccdc9a6432a980aacd9ac
Mayank Chugh <mayank.chugh@automattic.com>
2022-08-30 18:56:35 +0530

    QUALITY-51: testing playwright instrumentation in datadog

--- __tests__/e2e/playwright.config.ts
+++ __tests__/e2e/playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
     retries: 1,
     globalSetup: require.resolve( './lib/global-setup' ),
     timeout: 120000,
-    reporter: process.env.CI ? 'github' : 'list',
+    reporter: process.env.CI ? [ [ 'github' ], [ 'junit', { outputFile: 'e2eResults.xml' } ] ] : 'list',
     reportSlowTests: null,
     workers: 4,
     use: {
